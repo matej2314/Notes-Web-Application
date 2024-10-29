35173a3f92628ae1d571a68137ee509f1e91e60f
matej2314 <mateo2314@gmail.com>
2024-10-29 19:19:15 +0100

    .

--- src/backend/db.js
+++ src/backend/db.js
@@ -3,7 +3,7 @@ const logger = require('./logger');
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-	host: process.env.DB_HOST || '185.170.196.107' || 'http://185.170.196.107',
+	host: process.env.DB_HOST,
 	port: process.env.DB_PORT,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
